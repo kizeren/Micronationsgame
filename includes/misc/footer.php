1a97612d213abf80d2a02c851b15593db0be2304
Shaun Mcbride <shaun.mcbride@gmail.com>
2014-12-20 16:49:01 -0600

    Copyright stuff

--- includes/misc/footer.php
+++ includes/misc/footer.php
@@ -1,7 +1,7 @@
 <center>
 
 
-Copy right &copy; Shaun Mcbride 2011, 2012<br>
+Copy right &copy; Shaun Mcbride 2011, 2012, 2013, 2014<br>
 
 <br>
 Flag by: <a href="www.IconDrawer.com">IconDrawer</a></center>
